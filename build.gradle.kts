2c4e1e552eaf18abe0280cb35042f0457d23ee23
PokerS <2468624523@qq.com>
2021-12-20 14:02:21 +0800

    remove jcenter, update version for mirai-console.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,21 @@
 plugins {
-    val kotlinVersion = "1.4.31"
+    val kotlinVersion = "1.4.30"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.6.4"
+    id("net.mamoe.mirai-console") version "2.8.2"
 }
 
 group = "org.example"
-version = "0.1.0"
+version = "1.1.1"
 
 repositories {
     mavenLocal()
     maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
     implementation("com.alibaba:fastjson:1.2.78")
-    implementation("com.cloudconvert:cloudconvert-java:1.0.7")
+    implementation("com.cloudconvert:cloudconvert-java:1.1.0")
 }
